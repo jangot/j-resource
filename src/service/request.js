add4f5f4e0e7d43a2a0d10dec70df0f32e8be348
p.pulin <p.pulin@semrush.com>
2016-02-19 13:54:12 +0300

    use lib for request

--- src/service/request.js
+++ src/service/request.js
@@ -1,88 +1,28 @@
 "use strict";
 
-var http = require('http');
-var url = require('url');
-
 var HttpClientFactory = require("http-client-factory");
 var _ = require('lodash');
 
-var methodsWithBody = [
-    'POST',
-    'PUT'
-];
-
 var clientParamNames = [
-    'protocol', 'host', 'port', 'path'
-
+    'protocol',
+    'host',
+    'hostname',
+    'family',
+    'port',
+    'localAddress',
+    'socketPath',
+    'method',
+    'path',
+    'headers',
+    'auth',
+    'agent'
 ];
-
-function buildRequest(options) {
-    var method = (options.method || 'get').toLowerCase();
-    var params = options.params || {};
-    var headers = options.headers || {};
-
-
-    var clientParams = _.pick(options, clientParamNames);
-
-    var client = HttpClientFactory.getClient(clientParams);
-
-
-}
 
 module.exports = function(options) {
+    var params = options.params || {};
+    var requestOptions = _.pick(options, clientParamNames);
 
-    var method = (options.method || 'get').toLowerCase();
     var client = HttpClientFactory.getClient();
 
-    return client[method]();
-
-    var path = url.parse(options.path);
-
-    options.headers = options.headers || {};
-
-    return new Promise((resolve, reject) => {
-        function callback(response) {
-            var str = '';
-
-            response.on('data', function (chunk) {
-                str += chunk;
-            });
-
-            response.on('end', function () {
-                var result = {
-                    response: response,
-                    body: str
-                };
-
-                if (response.statusCode == 200) {
-                    resolve(result);
-                } else {
-                    reject(result);
-                }
-            });
-
-            response.on('error', function(e) {
-                reject(e);
-            });
-        }
-
-        var postData;
-        if (methodsWithBody.indexOf(options.method) > -1) {
-            var data = options.params || {};
-            postData = JSON.stringify(data);
-            options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-            options.headers['Content-Length'] = postData.length;
-        } else {
-            data =
-            options.path = url.format(path.search)
-        }
-
-        var request = http.request(options, callback);
-
-        if(postData) {
-            request.write(postData);
-        }
-
-        request.end();
-    });
+    return client.send(requestOptions, params);
 };
