e36e789a8dc5c8fc2ec4ab7fbe2864cf2db2f667
p.pulin <p.pulin@semrush.com>
2016-02-29 19:21:17 +0300

    add paramsToQuery

--- src/service/interceptors/paramsToQuery.js
+++ src/service/interceptors/paramsToQuery.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var debug = require('../debug');
+var url = require('url');
 
 var requestsForQuery = [
     'GET', 'HEAD'
@@ -8,15 +9,18 @@ var requestsForQuery = [
 
 module.exports = {
     request: function(config) {
-        if (requestsForQuery.indexOf(config.method) === -1) {
+        var method = config.method || 'GET';
+        if (requestsForQuery.indexOf(method) === -1) {
             return config;
         }
 
         var params = config.params || {};
         delete config.params;
 
-        var qs = config.qs || {};
-        config.qs = Object.assign(qs, params);
+        var qs = config.uri.query || {};
+        config.uri.search = url.format({
+            query: Object.assign(qs, params)
+        });
 
         debug.log('interceptor, paramsToQuery', config);
         return config;
